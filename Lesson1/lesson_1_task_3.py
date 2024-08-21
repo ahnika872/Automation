4a62dc753a98ef5ebebd5333f4e02fca3572771a
Veronika Ostanina <veronikaostanina@Noutbuk-Veronika.local>
2024-08-21 12:06:13 +0300

    first homework

---
+++ Lesson1/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+user_name = input('Ваше имя: ')
+user_age = int(input('Ваш возраст: '))
+print(f'Имя пользователя: {user_name}\nВозраст:{user_age}')
---
+++ Lesson1/lesson_1_task_7.py
@@ -0,0 +1,5 @@
+def print_number(num):
+    print(num, end = '')
+for i in [8, 8, 0, 0, 5, 5, 5, 3, 5, 3, 5]:
+    print_number(i)
+print()
