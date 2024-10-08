3beffb3a6f286d38ec51531cd4b2452a244d8bb7
Veronika Ostanina <veronikaostanina@Noutbuk-Veronika.local>
2024-10-08 22:50:03 +0300

    Вернула третью задачу, она была правильной

--- 06_lesson/l6(3).py
+++ 06_lesson/l6(3).py
@@ -2,7 +2,6 @@
 # Дождитесь загрузки всех картинок.
 # Получите значение атрибута srcу 3-й картинки.
 # Выведите значение в консоль.
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -15,13 +14,9 @@ waiter = WebDriverWait(driver, 20)
 
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-images = waiter.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#image-container img")))
+waiter.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#image-container img")))
+third_image = waiter.until(EC.presence_of_element_located((By.ID, "award")))
+src_value = third_image.get_attribute("src")
 
-if len(images) >= 3:
-    third_image = images[2]
-    src_value = third_image.get_attribute("src")
-    print(src_value)
-else:
-    print("Not enough images loaded")
-
+print(src_value)
 driver.quit()
